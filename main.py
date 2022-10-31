9c382f4db7eefca2a6d787955cffe6b6c0de9db9
duyvo1234 <91549340+duyvo1234@users.noreply.github.com>
2022-10-31 22:41:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from types import NoneType
 import pygame, sys
 
-# from boss import *
+from boss import *
 # from map_game import *
 # from bullet_icon import *
 from fighter import *
@@ -24,9 +25,15 @@ FPS = 60
 level = Level(level_map, screen)
 # ---------------MAP------------
 
-player = fighter(300, 300, 5, 1.5)
+player = fighter(200, 300, 5, 1.5)
+boss = boss(350, 300, 5, 2, screen)
+boss_attack = boss.boss_attack
+
+#Key event
 run_left = False
 run_right = False
+bossAttack = False
+skill = [False, False, False]
 
 run = True
 while run:
@@ -36,8 +43,13 @@ while run:
     screen.fill('black')
     level.run()
     # ---------------MAP------------
-    
-    img = player.show(screen)
+    #img = player.show(screen)
+    img2 = boss.show(screen)
+
+    #boss attack
+    boss_attack.update()
+    boss_attack.draw(screen)
+    #boss.ai(player, boss_attack)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -52,14 +64,21 @@ while run:
                 run_right = True
             if event.key == pygame.K_UP:
                 player.jump()
+            if event.key == pygame.K_SPACE:
+                boss.attack(0)
+            if event.key == pygame.K_DOWN:
+                boss.attack(1) 
                 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 run_left = False
             if event.key == pygame.K_RIGHT:
                 run_right = False
+            if event.key == pygame.K_SPACE:
+                bossAttack = False
         
-    state = player.update(run_left, run_right, level_map)
+    #state = player.update(run_left, run_right, level_map)
+    state = boss.update(run_left, run_right ,level_map)
     
     pygame.display.update()
     
